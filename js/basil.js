8ab3d8e0e6b76ec6e60fa291d3bf8968f849887e
stefanmos <iamstefanmos@gmail.com>
2016-04-19 21:19:38 +0200

    Added Animations, Equalizer & updated SASS

--- js/basil.js
+++ js/basil.js
@@ -20,6 +20,9 @@
 }).call(this);
 
 
+
+
+
 // Tabs
 
 $(document).ready(function(){
@@ -35,3 +38,68 @@ $(document).ready(function(){
   })
 
 })
+
+
+
+
+
+// animate.css
+
+$.fn.extend({
+    animateCss: function (animationName) {
+        var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+        $(this).addClass('animated ' + animationName).one(animationEnd, function() {
+            $(this).removeClass('animated ' + animationName);
+        });
+    }
+});
+
+$('#Animate-1').animateCss('fadeIn');
+$('#Animate-2').animateCss('fadeIn');
+$('#Animate-3').animateCss('fadeIn');
+
+
+
+
+// Equalizer
+/* Thanks to CSS Tricks for pointing out this bit of jQuery http://css-tricks.com/equal-height-blocks-in-rows/ */
+
+equalheight = function(equalize){
+
+var currentTallest = 0,
+     currentRowStart = 0,
+     rowDivs = new Array(),
+     $el,
+     topPosition = 0;
+ $(equalize).each(function() {
+
+   $el = $(this);
+   $($el).height('auto')
+   topPostion = $el.position().top;
+
+   if (currentRowStart != topPostion) {
+     for (currentDiv = 0 ; currentDiv < rowDivs.length ; currentDiv++) {
+       rowDivs[currentDiv].height(currentTallest);
+     }
+     rowDivs.length = 0; // empty the array
+     currentRowStart = topPostion;
+     currentTallest = $el.height();
+     rowDivs.push($el);
+   } else {
+     rowDivs.push($el);
+     currentTallest = (currentTallest < $el.height()) ? ($el.height()) : (currentTallest);
+  }
+   for (currentDiv = 0 ; currentDiv < rowDivs.length ; currentDiv++) {
+     rowDivs[currentDiv].height(currentTallest);
+   }
+ });
+}
+
+$(window).load(function() {
+  equalheight('.equalize div');
+});
+
+
+$(window).resize(function(){
+  equalheight('.equalize div');
+});
